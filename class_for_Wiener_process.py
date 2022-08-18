b8e2d70852fc379b38e1170b9228cd2b8dc9d26f
Lera-PR <60518906+Lera-PR@users.noreply.github.com>
2022-08-18 20:44:18 +0100

    Add files via upload

---
+++ class_for_Wiener_process.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import random
+import math
+import matplotlib.pyplot as plt
+
+
+# In[18]:
+
+
+class Wiener_Process:
+    def __init__(self,T,m):
+        W=[0]
+        S=0
+        delta_t=T/m
+        for i in range(0,m):
+            inc=random.gauss(0,delta_t)
+            S=S+inc
+            W.append(S)
+        self.W=W
+    
+    def print(self):
+        l=len(self.W)
+        for i in range(0,l):
+            print(self.W[i])
+        
+    def plot(self):
+        fig = plt.figure()
+        plt.plot(self.W)
+        plt.grid()
+
+
+# In[21]:
+
+
+T=2
+m=1000
+My_Wiener=Wiener_Process(T,m)
+My_Wiener.plot()
+
+
+# In[ ]:
+
+
+
+
