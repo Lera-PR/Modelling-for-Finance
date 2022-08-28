7214613d64875a2a32f6303aa09eddbeba42b8f3
Lera-PR <60518906+Lera-PR@users.noreply.github.com>
2022-08-28 11:43:52 +0100

    Add files via upload

---
+++ MLE prediction of future stock prices.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[105]:
+
+
+#Here I will use homemade MLE to predict future stock prices for TESLA
+import numpy as np
+import random
+import math
+import pandas as pd
+import matplotlib.pyplot as plt
+#df = pd.read_csv('TSLA.csv')
+#saved_column = df.column_name #you can also use df['column_name']
+
+
+# In[106]:
+
+
+df = pd.read_csv('TSLA.csv')
+closing_prices = df.Close #here we have closing prices of TESLA stock
+plt.plot(closing_prices)
+plt.grid()
+
+
+# In[107]:
+
+
+#apply log-transformation to get to a simple Brownian motion
+l_closing_prices=np.zeros([len(closing_prices),1])
+for i in range(len(closing_prices)):
+    l_closing_prices[i]=math.log(closing_prices[i])
+
+
+# In[112]:
+
+
+m=len(closing_prices)
+delta_t=1
+mu=0
+mu=(l_closing_prices[m-1] - l_closing_prices[0])/(m*delta_t) #MLE of historical drift mu
+
+sqsigma=0
+for i in range(0,m-1):
+    sqsigma=sqsigma+np.power(l_closing_prices[i+1]-l_closing_prices[i]-mu*delta_t,2)
+
+sqsigma=sqsigma/(m*delta_t) #MLE of historical volatility sigma^2
+sigma=math.sqrt(sqsigma)
+print(mu,sigma)
+
+#now we will predict future prices
+
+
+# In[139]:
+
+
+#now we will predict future prices
+n=70 #number of periods we predict prices for
+N=7
+X=np.zeros([n,1])
+plt.plot(closing_prices[1700:]) #I plot only part of the closing prices to make the picture nicer
+for i in range(0,N): #N Monte Carlo runs
+    X[0]=random.gauss(l_closing_prices[-1]+mu*delta_t,sqsigma*delta_t)
+    for j in range(1,n):
+        X[j]=random.gauss(X[j-1]+mu*delta_t,sqsigma*delta_t) #we know conditional distribution of X_t+1 on X_t
+    X=np.exp(X) #X will store predicted prices
+    plt.plot(range(m,m+n),X,'r')
+
+plt.grid()
+plt.show()
+    
+
+
+# In[ ]:
+
+
+
+
