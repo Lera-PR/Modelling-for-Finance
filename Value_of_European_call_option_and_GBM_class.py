bd58a7366ed1cfb0ad4561d0532239a8ecdfe349
Lera-PR <60518906+Lera-PR@users.noreply.github.com>
2022-09-06 13:13:40 +0100

    Update Value_of_European_call_option_and_GBM_class.py

--- Value_of_European_call_option_and_GBM_class.py
+++ Value_of_European_call_option_and_GBM_class.py
@@ -1,18 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[154]:
-
-
 import random
 import math
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import norm
 from mpl_toolkits import mplot3d
-
-
-# In[155]:
 
 
 class Wiener_Process:
@@ -54,81 +45,53 @@ class Geometric_Wiener_Process:
         plt.plot(x,self.S,'b')
         plt.grid()
         
-class Saving_Account:
-    def __init__(self,r,T,m):
-        self.T=T
-        self.r=r
-        self.m=m
-        delta_t=T/m
-        M=np.ones([m,1])
-        for i in range(1,m):
-            M[i]=M[i-1]*math.exp(r*delta_t)
-        self.M=M
-        
-    def plot(self):
-        x=np.linspace(0,self.T,self.m)
-        plt.plot(x,self.M,'r')
-        plt.grid()
+
         
 
-
-# In[156]:
-
-
-T=1
+T=1 #these are parameters for European call option
 m=1000
-d=T/m # step
+d=T/m # time step
 sigma=0.4
 K=10 #strike price
 S0=10
-
-
-# In[157]:
-
-
 r=0.05
-My_W=Wiener_Process(T,m)
 
-My_GBM=Geometric_Wiener_Process(My_W,r,sigma,S0)
+My_W=Wiener_Process(T,m) #create a Wiener process 
+
+My_GBM=Geometric_Wiener_Process(My_W,r,sigma,S0) #and create GBM based on the process and parameters
 My_GBM.plot()
-
-
-# In[172]:
 
 
 def F(d):
     return norm.cdf(d) #this is a cumulitive function of a standard normal distribution
 
-def Delta_of_European_call_option(t,S,K,m,r,sigma,T):
-    d1=(np.log(S/K)+(T-t)*(r+0.5*sigma**2))/(sigma*np.sqrt(T-t))
-    return F(d1)
+def Delta_of_European_call_option(t,S,K,m,r,sigma,T):            # calculate Delta of European_call_option given current time
+    d1=(np.log(S/K)+(T-t)*(r+0.5*sigma**2))/(sigma*np.sqrt(T-t)) #t, current price of stock S, and specified parameters
+    return F(d1)                                                 #of the option
 
-def Gamma_of_European_call_option(t,S,K,m,r,sigma,T):
-    d1=(np.log(S/K)+(T-t)*(r+0.5*sigma**2))/(sigma*np.sqrt(T-t))
-    Gamma=np.exp(-d1**2/2)/(S*sigma*np.sqrt(T-t)*np.sqrt(2*math.pi))
+def Gamma_of_European_call_option(t,S,K,m,r,sigma,T):                 # calculate Gamma of European_call_option given current time
+    d1=(np.log(S/K)+(T-t)*(r+0.5*sigma**2))/(sigma*np.sqrt(T-t))      #t, current price of stock S, and specified parameters
+    Gamma=np.exp(-d1**2/2)/(S*sigma*np.sqrt(T-t)*np.sqrt(2*math.pi))  #of the option
     return Gamma
 
-def Value_of_European_call_option(t,S,K,m,r,sigma,T):
-    d1=(np.log(S/K)+(T-t)*(r+0.5*sigma**2))/(sigma*np.sqrt(T-t))
-    d2=d1-sigma*np.sqrt(T-t)
+def Value_of_European_call_option(t,S,K,m,r,sigma,T):             # calculate value of European_call_option given current time
+    d1=(np.log(S/K)+(T-t)*(r+0.5*sigma**2))/(sigma*np.sqrt(T-t))  #t, current price of stock S, and specified parameters
+    d2=d1-sigma*np.sqrt(T-t)                                      #of the option
     V=S*F(d1)-K*np.exp(-r*(T-t))*F(d2)
     return V
 
 
-# In[169]:
-
-
-fig = plt.figure()
-ax = plt.axes(projection='3d')
+fig = plt.figure()                                                #here we calculate and plot values of European call 
+ax = plt.axes(projection='3d')                                    #option given GBM for stock prices
 t=np.linspace(d,T-d,m)
 V=np.zeros([m,1])
 for i in range(0,m):
     V[i]=Value_of_European_call_option(t[i],My_GBM.S[i],K,m,r,sigma,T)
-ax.scatter(t, My_GBM.S, V,color='red')
+ax.scatter(t, My_GBM.S, V,color='red')                                
 
-S_max=22
-t=np.linspace(d,T-d,m)
-S=np.linspace(0.01,S_max,m)
+S_max=22                                                         #here we calculate and plot values of European call
+t=np.linspace(d,T-d,m)                                           #option for all pairs (t,S) where t<T and S is a stock
+S=np.linspace(0.01,S_max,m)                                      #price
 X,Y = np.meshgrid(t, S)
 V=np.zeros([m,m])
 for i in range(0,m):
@@ -142,23 +105,19 @@ ax.set_zlabel('V(t,S(t))');
 
 ax.view_init(20, -120)
 
-#add plot of my geometric brownian motion 
 
 
-# In[170]:
-
-
-fig = plt.figure()
-ax = plt.axes(projection='3d')
+fig = plt.figure()                                              #here we calculate and plot Deltas of European call
+ax = plt.axes(projection='3d')                                  #option given GBM for stock prices
 t=np.linspace(d,T-d,m)
 D=np.zeros([m,1])
 for i in range(0,m):
     D[i]=Delta_of_European_call_option(t[i],My_GBM.S[i],K,m,r,sigma,T)
 ax.scatter(t, My_GBM.S, D,color='red')
 
-S_max=22
-t=np.linspace(d,T-d,m)
-S=np.linspace(0.01,S_max,m)
+S_max=22                                                        #here we calculate and plot Deltas of European call
+t=np.linspace(d,T-d,m)                                          #option for all pairs (t,S) where t<T and S is a stock
+S=np.linspace(0.01,S_max,m)                                     #price
 X,Y = np.meshgrid(t, S)
 D=np.zeros([m,m])
 for i in range(0,m):
@@ -172,23 +131,18 @@ ax.set_zlabel('Delta');
 
 ax.view_init(20, -120)
 
-#add plot of my geometric brownian motion 
 
-
-# In[174]:
-
-
-fig = plt.figure()
-ax = plt.axes(projection='3d')
+fig = plt.figure()                                               #here we calculate and plot Gammas of European call
+ax = plt.axes(projection='3d')                                   #option given GBM for stock prices
 t=np.linspace(d,T-d,m)
 Gamma=np.zeros([m,1])
 for i in range(0,m):
     Gamma[i]=Gamma_of_European_call_option(t[i],My_GBM.S[i],K,m,r,sigma,T)
 ax.scatter(t, My_GBM.S, Gamma,color='red')
 
-S_max=22
-t=np.linspace(d,T-d,m)
-S=np.linspace(0.01,S_max,m)
+S_max=22                                                         #here we calculate and plot Gammas of European call
+t=np.linspace(d,T-d,m)                                           #option for all pairs (t,S) where t<T and S is a stock
+S=np.linspace(0.01,S_max,m)                                      #price
 X,Y = np.meshgrid(t, S)
 Gamma=np.zeros([m,m])
 for i in range(0,m):
@@ -202,8 +156,6 @@ ax.set_zlabel('Gamma');
 
 ax.view_init(20, -120)
 
-
-# In[ ]:
 
 
 
