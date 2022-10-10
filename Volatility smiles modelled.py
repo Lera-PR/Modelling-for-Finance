10f8df2beee0a09072ca7887ee487dbf2c157342
Lera-PR <60518906+Lera-PR@users.noreply.github.com>
2022-10-10 18:03:05 +0100

    Add files via upload

---
+++ Volatility smiles modelled.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime as dt
+from mpl_toolkits import mplot3d
+
+
+# In[25]:
+
+
+nflx_option = pd.read_csv("netflix_options.csv")
+#print(nflx_option)
+nflx_option["Expiration Date"] = pd.to_datetime(nflx_option["Expiration Date"])
+
+dt.date(2019, 4, 8) #let's say it is a today date
+
+nflx_option["timeToMaturity"] = (nflx_option["Expiration Date"].dt.date - dt.date(2019, 4, 8))/ dt.timedelta(days=1)
+#here we added an additional column, calculating time to maturity
+
+x = nflx_option["timeToMaturity"]
+y = nflx_option["Strike"]
+z = nflx_option["IV"]
+fig= plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+ax.set_xlabel('time to maturity')
+ax.set_ylabel('strike')
+ax.set_zlabel('Implied vol')
+ax.scatter(x, y, z, c='red', marker='o')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
